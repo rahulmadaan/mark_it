8ef00ea63f33d04ddc117d86b47a159e4d311f0f
Rahul Madaan <rahulma@thoughtworks.com>
2019-09-14 12:38:54 +0530

    Add simple bookmark page which is showing categorized and uncategorizeed bookmarks

---
+++ src/bookmarks.js
@@ -0,0 +1,52 @@
+const createNewCategory = title => console.log(title);
+const isBookmarkUncategorized = bookmark => !bookmark.children;
+
+const getAchorTag = bookmark => {
+  const anchorTag = document.createElement("a");
+  anchorTag.innerHTML = bookmark.title;
+  anchorTag.href = bookmark.url;
+  return anchorTag;
+};
+
+const setupUncategorizedList = bookmark => {
+  const bookmarksList = document.getElementById(
+    "uncategorized-bookmarks-fixed"
+  );
+  const anchorTag = getAchorTag(bookmark);
+
+  const newBookmark = document.createElement("div");
+  newBookmark.className = "uncategorized-bookmark";
+  newBookmark.appendChild(anchorTag);
+
+  bookmarksList.appendChild(newBookmark);
+};
+
+const createCategorizedBookmarks = bookmarkList => {
+  const newCategory = document.createElement("div");
+  newCategory.className = "category-new";
+  bookmarkList.children.map(bookmark => {
+    const newBookmark = document.createElement("div");
+    const anchorTag = getAchorTag(bookmark);
+    newBookmark.className = "categorized-bookmark";
+    newBookmark.appendChild(anchorTag);
+    newCategory.appendChild(newBookmark);
+  });
+  document
+    .getElementById("categorized-bookmarks-fixed")
+    .appendChild(newCategory);
+};
+
+const loadBookmarks = () => {
+  chrome.bookmarks.getTree(output => {
+    const bookmarks = output[0].children[0].children;
+
+    bookmarks.map(bookmark => {
+      if (isBookmarkUncategorized(bookmark)) {
+        setupUncategorizedList(bookmark);
+      } else {
+        createCategorizedBookmarks(bookmark);
+      }
+    });
+  });
+};
+loadBookmarks();
