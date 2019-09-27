f5728c2b250698f3de6e8c5b0ca325d36686fa53
Rahul Madaan <rahulma@thoughtworks.com>
2019-09-27 22:39:22 +0530

    Logging current browser activity in localstorage

--- src/activity.js
+++ src/activity.js
@@ -1,9 +1,49 @@
+const getPastData = () => {
+  const data = localStorage.getItem("activity");
+  return JSON.parse(data);
+};
+
+const isUrlExists = url => {
+  const pastData = getPastData();
+  return pastData.filter(entry => entry.url == url)[0];
+};
+
+const updateLocalData = data => {
+  localStorage.setItem("activity", JSON.stringify(data));
+};
+
+const incrementVote = url => {
+  const pastData = getPastData();
+  pastData.map(tab => {
+    if (tab.url == url) {
+      tab.count = tab.count + 1;
+    }
+  });
+  updateLocalData(pastData);
+};
+
+const addNewEntry = url => {
+  const data = getPastData();
+  data.push({ url: url, count: 0 });
+  localStorage.setItem("activity", JSON.stringify(data));
+};
+
+const updateActivityData = url => {
+  if (isUrlExists(url)) {
+    incrementVote(url);
+    return;
+  }
+  addNewEntry(url);
+};
+
 const main = () => {
-  chrome.tabs.getAllInWindow(tabs => {
-    tabs.map(tab => {
-      if (tab.active) {
-        console.log("url is", tab.url);
-      }
+  chrome.tabs.getCurrent(current => {
+    chrome.tabs.getAllInWindow(tabs => {
+      tabs.map(tab => {
+        if (current && tab.active) {
+          updateActivityData(tab.url);
+        }
+      });
     });
   });
 };
@@ -13,7 +53,14 @@ const initializeActivity = () => {
   }
 };
 
-initializeActivity();
+const initialize = () => {
+  initializeActivity();
+  setInterval(() => {
+    main();
+  }, 1000);
+};
+initialize();
+
 //-----------sampleJSON-------------------
 [
   {
