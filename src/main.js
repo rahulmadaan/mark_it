519322b579706034095c24b3029a0a9dc5aeb990
Rahul Madaan <rahulma@thoughtworks.com>
2019-10-19 23:02:25 +0530

    Images can now be saved to chrome local storage

--- src/main.js
+++ src/main.js
@@ -12,7 +12,15 @@ const setUserName = userName => {
     "greeting"
   ).innerText = `${greetingPrefix} \t ${userName}`;
 };
-
+const updateImages = newImage => {
+  chrome.storage.local.get("background-images", images => {
+    images["background-images"].push(newImage);
+    chrome.storage.local.set(
+      { "background-images": images["background-images"] },
+      out => {}
+    );
+  });
+};
 const clearInputField = () => {
   document.getElementById("name-input").value = "";
 };
@@ -27,13 +35,13 @@ const set = () => {
 const bindEventListeners = () => {
   document.getElementById("name-input-button").addEventListener("click", set);
 
-  document.body.addEventListener("click", e => {
-    if (e.target.id !== "setting-div") {
-      document.getElementById("setting-popup").style.width = "0%";
-    } else {
-      document.getElementById("setting-popup").style.width = "50%";
-    }
+  document.getElementById("setting-div").addEventListener("click", e => {
+    document.getElementById("setting-popup").style.width = "50%";
   });
+
+  document
+    .getElementById("inputFileToLoad")
+    .addEventListener("change", uploadImage);
 };
 const displayUser = () => {
   const userName = localStorage.getItem("userName");
@@ -42,9 +50,39 @@ const displayUser = () => {
   }
 };
 
+const setupStorage = () => {
+  chrome.storage.local.get("background-images", images => {
+    if (Object.keys(images).length < 1) {
+      chrome.storage.local.set({ "background-images": [] }, a =>
+        console.log(a)
+      );
+    }
+  });
+};
+
+const addImage = src => {
+  const newImage = {
+    src,
+    isActive: false
+  };
+  updateImages(newImage);
+};
+const uploadImage = function() {
+  var filesSelected = document.getElementById("inputFileToLoad").files;
+  var fileToLoad = filesSelected[0];
+  var fileReader = new FileReader();
+  fileReader.readAsDataURL(fileToLoad);
+
+  fileReader.onload = fileLoadedEvent => {
+    var srcData = fileLoadedEvent.target.result;
+    addImage(srcData);
+  };
+};
+
 const initialize = () => {
   displayUser();
   bindEventListeners();
+  setupStorage();
 };
 
 window.onload = initialize;
